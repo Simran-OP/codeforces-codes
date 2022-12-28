283f22939409f2e69068a65f531bcd95fef18667
simran <simransaini21102000@gmail.com>
2022-12-28 16:10:45 +0530

    roundfinal28/12/22

--- RoundDownPrice.cpp
+++ RoundDownPrice.cpp
@@ -17,10 +17,23 @@
 //             //cout<<val<<" "<<k<<endl;
 //         }while(val!=0);
 //             cout<<n-(pow(10,k-1))<<endl;
-        
+
 //     }
 //     return 0;
 // }
 
 #include <bits/stdc++.h>
-using namespace std;int main(){int t;cin>>t;while(t--){string s;cin>>s;s[0]--;cout<<atoi(s.c_str())<<endl;} return 0;}
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        s[0]--;
+        cout << atoi(s.c_str()) << endl;
+    }
+    return 0;
+}
